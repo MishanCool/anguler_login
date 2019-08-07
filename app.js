c6d07a0e32aa40faf5656cc3fd59e49ad97ef356
MishanCool <mishancool2016.com>
2019-08-07 22:53:13 +0530

    Create app variable and set up port

--- app.js
+++ app.js
@@ -4,3 +4,11 @@
   const cors = require('cors');
   const passport = require('passport');
   const mongoose = require('mongoose');
+
+  const app = express();
+
+  const port = 3000;
+
+  app.listen(port, () => {
+    console.log('Server started on port '+port);
+  });
