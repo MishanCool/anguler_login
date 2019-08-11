5839b1e1efc72e96fb98ee0eb5352210756fe6f5
Mishan Madhupa <mishancool2016@gmail.com>
2019-08-11 10:54:26 +0530

    Create variable name UserSchema and set attributes(name,email,username,password)

--- models/user.js
+++ models/user.js
@@ -1,3 +1,26 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');//bcrypt is password hashing algorithem
 const config = require('../models/database');
+
+//User Schema
+const UserSchema = mongoose.Schema({
+
+    name:{
+        type: String
+    },
+
+    email:{
+        type: String,
+        required: true
+    },
+
+    username: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+});
