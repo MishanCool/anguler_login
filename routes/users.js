33b9cbf2444b124e107d9cb9434673603caeadb8
Mishan Madhupa <mishancool2016@gmail.com>
2019-08-12 23:05:12 +0530

    Bring passport,jwt and User modules

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 //Register
 router.get('/register', (req, res, next) => {
