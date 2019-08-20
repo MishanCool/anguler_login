bcc42cf93b00156b8935d62ec498e829ba1ed95e
Mishan Madhupa <mishancool2016@gmail.com>
2019-08-20 23:56:53 +0530

    Protect a route

--- routes/users.js
+++ routes/users.js
@@ -72,7 +72,7 @@ router.post('/authenticate', (req, res, next) => {
 });
 
 //Profile
-router.get('/profile', (req, res, next) => {
+router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
     res.send('PROFILE');
 });
 
