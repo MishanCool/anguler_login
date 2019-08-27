0ef45d5f69ed088a5b02bba8c50cad41833f6f10
Mishan Madhupa <mishancool2016@gmail.com>
2019-08-27 06:11:37 +0530

    Create routes

--- angularLogin/src/app/app.module.ts
+++ angularLogin/src/app/app.module.ts
@@ -1,16 +1,38 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+const appRoutes: Routes = [
+  {path: '' , component: HomeComponent},
+  {path: 'login' , component: LoginComponent},
+  {path: 'register' , component: RegisterComponent},
+  {path: 'dashboard' , component: DashboardComponent},
+  {path: 'profile' , component: ProfileComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
